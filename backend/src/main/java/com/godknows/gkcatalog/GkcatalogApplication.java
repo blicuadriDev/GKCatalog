bfaa2b9b92aa3f6be2789a8dd9b3d7de44226a02
blicuadriDev <blicuadriDev@gmail.com>
2023-08-23 10:15:12 -0300

    GKCatalog project started

---
+++ backend/src/main/java/com/godknows/gkcatalog/GkcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.godknows.gkcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/godknows/gkcatalog/GkcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godknows.gkcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
