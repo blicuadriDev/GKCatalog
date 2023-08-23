f40d05b31f2e4e1757d0651e8d048f071ae8ccd4
blicuadriDev <blicuadriDev@gmail.com>
2023-08-23 13:00:13 -0300

    CategoryDTO created and adjustments made to Resource and Service layer

--- backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
+++ backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.godknows.gkcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.godknows.gkcatalog.dtos.CategoryDTO;
 import com.godknows.gkcatalog.entities.Category;
 import com.godknows.gkcatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,17 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		//List<CategoryDTO> listDto = new ArrayList<>();
+		//for(Category cat : list) {
+		//	listDto.add(new CategoryDTO(cat));
+		//}
+		//return listDto;
+		// OR ELSE VENE BETTER AND SAME RESULT
+		
+		return  list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
