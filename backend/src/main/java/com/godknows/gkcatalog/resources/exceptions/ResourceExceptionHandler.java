8c5ebd4a83813a9e614248e05a4d8234dfaf76ee
blicuadriDev <blicuadriDev@gmail.com>
2023-08-23 14:50:22 -0300

    Delete Category endpoint created

--- backend/src/main/java/com/godknows/gkcatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/godknows/gkcatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,28 +5,45 @@ import java.time.Instant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.godknows.gkcatalog.services.exceptions.DatabaseException;
 import com.godknows.gkcatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
-	
-	public ResponseEntity<StandardError> entityNotFound (ResourceNotFoundException e, HttpServletRequest request){
-		
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
 		StandardError err = new StandardError();
-		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource Not Found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+
+		return ResponseEntity.status(status).body(err);
 	}
 	
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError();
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
 
 }
--- backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
+++ backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.godknows.gkcatalog.dtos.CategoryDTO;
 import com.godknows.gkcatalog.entities.Category;
 import com.godknows.gkcatalog.repositories.CategoryRepository;
+import com.godknows.gkcatalog.services.exceptions.DatabaseException;
 import com.godknows.gkcatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -64,6 +67,20 @@ public class CategoryService {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Resource ID " + id + " Not Found");
 		}
+	}
+
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+	        	repository.deleteById(id);    		
+		}
+	    	catch (DataIntegrityViolationException e) {
+	        	throw new DatabaseException("Falha de integridade referencial");
+	   	}
 	}
 
 	
