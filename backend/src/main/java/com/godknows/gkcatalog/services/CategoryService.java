f2a785f7539f4867c81710b19f0c20f40eb291d2
blicuadriDev <blicuadriDev@gmail.com>
2023-08-23 13:22:38 -0300

    Find Category by Id endpoint created

--- backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
+++ backend/src/main/java/com/godknows/gkcatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.godknows.gkcatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,14 @@ public class CategoryService {
 		// OR ELSE VENE BETTER AND SAME RESULT
 		
 		return  list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
