e23c0b22008d0e66054fcf07c529c161e3864954
blicuadriDev <blicuadriDev@gmail.com>
2023-08-28 12:13:46 -0300

    Just citing the use of Fixture to avoid repetition

--- backend/src/test/java/com/godknows/gkcatalog/cateories/ProductRepositoryTests.java
+++ backend/src/test/java/com/godknows/gkcatalog/cateories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.godknows.gkcatalog.cateories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,14 +14,21 @@ import com.godknows.gkcatalog.repositories.ProductRepository;
 @DataJpaTest
 public class ProductRepositoryTests {
 	
+	private Long existingId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+	}
+	
 	@Autowired
 	private ProductRepository repository;
 	
 	@Test
 	public void deteleShouldDeleteObjectWhenIdExist() {
 		
-		//Arrange
-		Long existingId = 1L;
+		//Arrange (I can remove this since I already set the id using Fixture @BeforeEach)
+		//Long existingId = 1L;
 		
 		//Act
 		repository.deleteById(existingId);
