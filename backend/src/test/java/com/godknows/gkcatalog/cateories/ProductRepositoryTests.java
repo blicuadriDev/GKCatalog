03e8156e58f07bbf86c63fecdcd6d7becaebf741
blicuadriDev <blicuadriDev@gmail.com>
2023-08-28 13:03:19 -0300

    FindById tests

--- backend/src/test/java/com/godknows/gkcatalog/cateories/ProductRepositoryTests.java
+++ backend/src/test/java/com/godknows/gkcatalog/cateories/ProductRepositoryTests.java
@@ -11,18 +11,38 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.godknows.gkcatalog.entities.Product;
 import com.godknows.gkcatalog.repositories.ProductRepository;
 
+import tests.ProductFactoryTests;
+
 @DataJpaTest
 public class ProductRepositoryTests {
 	
 	private Long existingId;
+	private Long unexistingId;
+	private Long countTotalProducts;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
+		unexistingId = 100L;
+		countTotalProducts = 25L;
 	}
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = ProductFactoryTests.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts +1, product.getId());;
+	}
+	
+	
 	
 	@Test
 	public void deteleShouldDeleteObjectWhenIdExist() {
@@ -35,6 +55,35 @@ public class ProductRepositoryTests {
 		
 		//Assert
 		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertFalse(result.isPresent());
+
+	}
+	
+	
+	@Test
+	public void findByIdShouldReurnAnOptionalNotEmptyWhenIdExists() {
+		
+		//Arrange (I can remove this since I already set the id using Fixture @BeforeEach)
+		//Long existingId = 1L;
+		
+		//Act
+		Optional<Product> result = repository.findById(existingId);
+		
+		//Assert
+		Assertions.assertTrue(result.isPresent());
+
+	}
+	
+	@Test
+	public void findByIdShouldReurnAnEmptyOptionalWhenIdDoesNotExist() {
+		
+		//Arrange (I can remove this since I already set the id using Fixture @BeforeEach)
+		//Long existingId = 1L;
+		
+		//Act
+		Optional<Product> result = repository.findById(unexistingId);
+		
+		//Assert
 		Assertions.assertFalse(result.isPresent());
 
 	}
